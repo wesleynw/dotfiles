423f42e0710434289e85847125de79af823d2573
Wesley Weisenberger <wesleynw@pm.me>
2022-03-04 14:25:01 -0800

    volume script

---
+++ scripts/volume.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# volume.sh
+
+# Arbitrary but unique message tag
+msgTag="myvolume"
+
+# Change the volume using alsa(might differ if you use pulseaudio)
+case $1 in 
+	up)
+		pactl set-sink-mute @DEFAULT_SINK@  false ; pactl set-sink-volume @DEFAULT_SINK@ +10%
+		;;
+	down)
+		pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ -10%
+		;;
+	mute)
+		# toggle
+		pactl set-sink-mute @DEFAULT_SINK@ toggle
+		;;
+esac
+
+# Query amixer for the current volume and whether or not the speaker is muted
+#volume="$(amixer -c 0 get Master | tail -1 | awk '{print $4}' | sed 's/[^0-9]*//g')"
+volume="$(pamixer --get-volume)"
+mute="$(pamixer --get-mute)"
+echo "deez nuts"
+
+if [[ $1 -ne "mute" || $mute == "false" ]]; then
+	dunstify -a "changeVolume" -u low -i audio-volume-high -h string:x-dunst-stack-tag:$msgTag \
+    	-h int:value:"$volume" "Volume: ${volume}%"
+else 
+	dunstify -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted" 
+fi
