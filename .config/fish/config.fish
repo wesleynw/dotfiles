097cd40bc266371e67f26b94ad551650cbbb3a44
Wesley Weisenberger <wesleynw@pm.me>
2025-11-25 22:42:54 -0500

    fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+zoxide init --cmd cd fish | source
+
+alias config '/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME'
+
