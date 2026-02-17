63cd6cdad35e4c724de97112e294df26f5bb1aef
Wesley Weisenberger <wesleynw@pm.me>
2026-02-17 09:17:56 -0500

    vi mode to zsh + add mise

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
 end
 
 set fish_greeting
+set -g fish_key_bindings fish_vi_key_bindings
 
 zoxide init --cmd cd fish | source
 
@@ -10,3 +11,4 @@ alias config '/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME'
 alias vim 'nvim'
 
 export PATH="$HOME/.local/bin:$PATH"
+/Users/wesleyweisenberger/.local/bin/mise activate fish | source
